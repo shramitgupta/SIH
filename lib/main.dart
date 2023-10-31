ad2264516224c4b019e1818177452fd95ccb171d
shramitgupta <shramitgupta1212@gmail.com>
2023-10-31 19:30:31 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:sih/firebase_options.dart';
 import 'package:sih/homescreen.dart';
 import 'package:sih/login.dart';
 import 'package:sih/splashscreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
